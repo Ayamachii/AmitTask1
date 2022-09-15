9c7b310d68000b95816c96b7d6a90488fc2a2538
Ayamachii <ayaeyad87@gmail.com>
2022-09-15 20:19:54 +0200

    UI only Talabat app

--- app/src/main/java/com/example/firsttask/MainActivity.java
+++ app/src/main/java/com/example/firsttask/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.firsttask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.example.firsttask.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
